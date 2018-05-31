02e67c50c70c9f16e981dc398e5515dc51d8f7e7
Unknown <1146923246@qq.com>
2018-05-31 10:22:47 +0800

    完成eventManager

---
+++ service/transactional-event/src/main/java/com/tongyuan/Event.java
@@ -0,0 +1,47 @@
+package com.tongyuan;
+
+import java.util.UUID;
+
+/**
+ * Created by zhangcy on 2018/5/31
+ */
+public class Event {
+
+    private String id;
+    private EventType eventType;
+    private String modelName;
+    private String modelId;
+    private long createdTime;
+
+    public Event() {
+    }
+
+    public Event(EventType eventType, String modelName, String modelId) {
+        this.id = UUID.randomUUID().toString();
+        this.eventType = eventType;
+        this.modelName = modelName;
+        this.modelId = modelId;
+        this.createdTime = System.currentTimeMillis();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public String getModelId() {
+        return modelId;
+    }
+
+    public long getCreatedTime() {
+        return createdTime;
+    }
+}
+
---
+++ service/transactional-event/src/main/java/com/tongyuan/EventType.java
@@ -0,0 +1,8 @@
+package com.tongyuan;
+
+/**
+ * Created by zhangcy on 2018/5/31
+ */
+public enum EventType {
+    INSERT,UPDATE,DELETE
+}
